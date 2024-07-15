08d43cfd299d4389efb0c1810ce2ad9022b2f4b4
Olexandr2024 <stepanenko.aleksandr@fmail.com>
2024-07-15 20:14:01 +0300

    git@github.com:Olexandr2024/Python_24.06.24.git

--- Lesson_5/Home_work_5/Main_5.py
+++ Lesson_5/Home_work_5/Main_5.py
@@ -146,13 +146,14 @@ print(f"Reversed list: {reversed_list}")
 
 # Task_12
 
-for num in range(2, 101):
-
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
+res = []
+for n in range(2, 100):
+    for i in range(2, n):
+        if n % i == 0:
             break
     else:
-        print(num)
+        res.append(n)
+print(res)
 
 
 
---
+++ Lesson_6/test.py
@@ -0,0 +1,10 @@
+n = 5
+
+if n > 1:
+    for i in range(2, n):
+        if n % i == 0:
+            print('Not prime')
+            break
+    else:
+        print('Prime')
+
